558cc78f67e944b12345deac4dc19c9414318ad0
Quinn Hart <qjhart@ucdavis.edu>
2018-02-16 10:22:52 -0800

    Added folder / card metadata.

--- ttl.js
+++ ttl.js
@@ -14,6 +14,7 @@ var prefix= {
 		dct: 'http://purl.org/dc/terms/',
 		ebucore: 'http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#',
 		foaf: 'http://xmlns.com/foaf/0.1/',
+		gv: 'http://google.com/google-vision/',
 //		p: 'http://www.wikidata.org/prop/',
 //		ps: 'http://www.wikidata.org/prop/statement/',
 //		pq: 'http://www.wikidata.org/prop/qualifier/',
@@ -28,8 +29,10 @@ var prefix= {
 
 let argv=yargs.demandCommand(1).argv;
 
-
 function add_ttl (dir) {
+		let errl=[];
+		let warnl=[];
+
 		let outfn=fs.createWriteStream(path.join(dir,"metadata.ttl"));
 		let writer=n3.Writer(outfn,{prefixes: prefix});
 
@@ -44,14 +47,35 @@ function add_ttl (dir) {
 				return writer.addTriple(s,p,o);
 		}
 
-
-		let m=jf.readFileSync(path.join(dir,"metadata.json"));
-		let v=jf.readFileSync(path.join(dir,"google-vision.json"));
+		let label_m={};
+		try {
+				label_m=jf.readFileSync(path.join(dir,"metadata.json"));
+		} catch (err) {
+				warnl.push(`${dir}/metadata.json`);
+		}
+		let index_m={};
+		try {
+				index_m=jf.readFileSync(path.join(dir,"..","metadata.json"));
+		} catch (err) {
+				warnl.push(`${dir}/../metadata.json`);
+		}
+		let m={};
+		try {
+				m=jf.readFileSync(path.join(dir,"..","..","metadata.json"));
+		} catch (err) {
+				warnl.push(`${dir}/../../metadata.json`);
+		}
+		let v;
+		try {
+				v=jf.readFileSync(path.join(dir,"google-vision.json"));
+		} catch (err) {
+				warnl.push(`google-vision.json`);
+		}
 		let l;
 		try {
 				l = jf.readFileSync(path.join(dir,"label_this.json"));
 		} catch (err) {
-				console.log (err,path.join(dir,"label_this.json"));
+				warnl.push(`label_this.json`);
 		}
 		add('','rdf:type','w:WineLabel');
     add('','ebucore:filename',`"${dir}.jpg`) ;
@@ -61,21 +85,73 @@ function add_ttl (dir) {
     add('','ebucore:hasMimeType','"image/jpeg"') ;
     add('','dc:identifier',`"${dir}"`);
     add('','dct:type','"image"');
-		m.keywords.forEach((k)=>{add('','dc:subject',`"${k}@en"`);});
-		let label_txt=v.text[0].desc ;
-		add('','w:bagOfWords',`"${label_txt}"@${m.language_id}`);
-		if (l.text[0].desc) {
-				label_txt=l.text[0].desc ;
+		// Now add all metadata together
+		let overwrite=['language_id','country_id'];
+
+		overwrite.forEach((k)=>{if (index_m[k]) { m[k]=index_m[k];}});
+		if (index_m.keywords) {
+				if (m.keywords) {
+						m.keywords.concat(index_m.keywords);
+				}
+		}
+		overwrite.forEach((k)=>{if (label_m[k]) { m[k]=label_m[k];}});
+		if (label_m.keywords) {
+				if (m.keywords) {
+						m.keywords.concat(label_m.keywords);
+				}
+		}
+
+		if (m.keywords) {
+				m.keywords.forEach((k)=>{add('','dc:subject',`"${k}@en"`);});
+		}
+		let label_txt;
+		try {
+				label_txt=v.text[0].desc ;
 				add('','w:bagOfWords',`"${label_txt}"@${m.language_id}`);
+		} catch (err) {
+				warnl.push(`google-vision.text[0]`);
+		}
+		try {
+				v.properties.colors.forEach((c)=>{
+						let lab=[];
+						['hex','red','blue','green','score','coverage']
+								.forEach((p)=>{ lab.push({"predicate":`gv:${p}`,'object':'"'+c[p]+'"'}); });
+						add('','gv:color',writer.blank(lab));
+				})
+		} catch(err) {
+				warnl.push(`google-vision.properties.color ${err}`);
+		}
+
+		try {
+				v.labels.forEach((c)=>{
+						let lab=[{
+								predicate:`gv:mid`,
+								object:'https://g.co/kg'+c.mid}];
+						['desc','score']
+								.forEach((p)=>{ lab.push({"predicate":`gv:${p}`,'object':'"'+c[p]+'"'}); });
+						add('','gv:label',writer.blank(lab));
+				})
+		} catch(err) {
+				warnl.push(`google-vision.labels ${err}`);
+		}
+
+		try {
+				label_txt=v.text[0].desc ;
+				add('','w:bagOfWords',`"${label_txt}"@${m.language_id}`);
+		} catch (err) {
+				warnl.push(`label_this.text[0]`);
 		}
 		if (m.language_id) {
 				add('','wdt:P219',`"${m.language_id}"`);
 		}
+		// Really this might want to be a wine node, but I'm not sure
+		// If we need to be that complicated
 		if (m.country_id || m.type ) {
-				let wine='#wine';
-				add('','w:Wine',wine);
+//				let wine='#wine';
+//				add('','w:Wine',wine);
 //				let wine=writer.createBlankNode();
-				add(wine,'rdf:type','w:Wine');
+				//				add(wine,'rdf:type','w:Wine');
+				let wine='';
 				if (m.type) {
 						add(wine,'w:WineType','w:'+_.capitalize(m.type));
 				}
@@ -87,6 +163,9 @@ function add_ttl (dir) {
 				}
 		}
 		writer.end();
+		if (errl.length>0 || warnl.length>0) {
+				console.log([dir,errl.join(' '),warnl.join(' ')].join(':'));
+		}
 //		writer.end(function (error, result) { console.log(result); });
 }
 
